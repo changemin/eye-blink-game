fbf86d1e2fdfba9def797505e397a0ac04047c21
Changemin <bkm.change.min@gmail.com>
2019-10-25 11:55:18 +0900

    remove rasbpi mode

--- main.py
+++ main.py
@@ -10,12 +10,7 @@ import random
 from EyeDetector import EyeDetector
 from Camera import Camera
 
-try:
-    from picamera.array import PiRGBArray
-    from picamera import PiCamera
-    isRaspi = True
-except ImportError:
-    import cv2
+import cv2
 
 WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 1920
@@ -60,33 +55,31 @@ def main():
                 # 임시 카운트 리셋
                 temp_count = 0
 
-            if not raspiMode:
                 # 화면에 눈의 크기 표시
-                cv2.putText(frame, "Score: {}".format(total_blink_count*100), (10, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-                cv2.putText(frame, "percent: {:.2f}".format(probability[0][0]), (300, 30),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(frame, "Score: {}".format(total_blink_count*100), (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+            cv2.putText(frame, "percent: {:.2f}".format(probability[0][0]), (300, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 
-                cv2.rectangle(frame, (left_eye_rect[0], left_eye_rect[1]), (
-                    left_eye_rect[2], left_eye_rect[3]), (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)), 3)
-                cv2.rectangle(frame, (right_eye_rect[0], right_eye_rect[1]), (
-                    right_eye_rect[2], right_eye_rect[3]), (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)), 3)
+            cv2.rectangle(frame, (left_eye_rect[0], left_eye_rect[1]), (
+                left_eye_rect[2], left_eye_rect[3]), (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)), 3)
+            cv2.rectangle(frame, (right_eye_rect[0], right_eye_rect[1]), (
+                right_eye_rect[2], right_eye_rect[3]), (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255)), 3)
 
-        if not raspiMode:
-            # 현재 프레임 보여줌
-            # cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
-            # cv2.setWindowProperty("Frame",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
-            dst = cv2.resize(frame, dsize=(WINDOW_WIDTH, WINDOW_WIDTH), interpolation=cv2.INTER_AREA)
-            cv2.imshow("Frame",dst)
+        # 현재 프레임 보여줌
+        # cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+        # cv2.setWindowProperty("Frame",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        dst = cv2.resize(frame, dsize=(WINDOW_WIDTH, WINDOW_WIDTH), interpolation=cv2.INTER_AREA)
+        cv2.imshow("Frame",dst)
 
-            key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(1) & 0xFF
 
-            # q 눌리면 프로그램 종료
-            if key == ord("q"):
-                # 띄운 윈도우와 비디오 스트리밍 종료
-                camera.release()
-                cv2.destroyAllWindows()
-                break
+        # q 눌리면 프로그램 종료
+        if key == ord("q"):
+            # 띄운 윈도우와 비디오 스트리밍 종료
+            camera.release()
+            cv2.destroyAllWindows()
+            break
 
 
 if __name__ == "__main__":
